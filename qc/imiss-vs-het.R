b579692c699464f2fd0538546118da30e4c836fd
Philip Law <philip.law@icr.ac.uk>
2019-11-28 16:02:14 +0000

    Update imiss-vs-het.R

--- qc/imiss-vs-het.R
+++ qc/imiss-vs-het.R
@@ -1,16 +1,14 @@
-library(tidyverse)
-
 #path to input data
 path=""
 #root of .bed/.bim/.fam files
 expername=""
 
-imiss=read.table(paste0(path,expername,".imiss"),h=T)
+imiss=read.table(paste0(path,"/",expername,".imiss"),h=T)
 imiss$logF_MISS = log10(imiss[,6])
-het=read.table(paste0(path,expername,".het"),h=T)
+het=read.table(paste0(path,"/",expername,".het"),h=T)
 het$meanHet = (het$N.NM. - het$O.HOM.)/het$N.NM.
 colors  <- densCols(imiss$logF_MISS,het$meanHet, nbin=128, bandwidth = c(0.5,0.5))
-pdf(paste0(path,expername,".imiss-vs-het.pdf"))
+pdf(paste0(path,"/",expername,".imiss-vs-het.pdf"))
 plot(imiss$logF_MISS,het$meanHet, col=colors, pch=20, ylim=c(0,0.5), xlim=c(-6,0), xlab="Proportion of missing genotypes", ylab="Heterozygosity rate",axes=F)
 axis(2,at=c(0,0.05,0.10,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5),tick=T)
 axis(1,at=-6:0, labels=10**(-6:0))
@@ -26,7 +24,7 @@ hetero1=het[which(het$meanHet<(m-3*stdev)),1]
 hetero2=het[which(het$meanHet>(m+3*stdev)),1]
 exclude=union(missing, hetero1)
 exclude=union(exclude, hetero2)
-fileConn<-file(paste0(path,"fail-imisshet-qc.txt"),"w")
+fileConn<-file(paste0(path,"/fail-imisshet-qc.txt"),"w")
 for (e in exclude){
   write(paste(e,e), fileConn, append=T)
 }
