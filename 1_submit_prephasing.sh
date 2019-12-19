f24c7642e843e12b49e28777e586e664c27d67b3
Philip Law <philip.law@icr.ac.uk>
2019-12-19 11:43:15 +0000

    Update 1_submit_prephasing.sh

--- 1_submit_prephasing.sh
+++ 1_submit_prephasing.sh
@@ -7,7 +7,13 @@
 # BSUB -o /scratch/DGE/MOPOPGEN/plaw/PRS/CRC/onco_with_rel/impute/phasing_%J_chr%I.stdout
 # BSUB -e /scratch/DGE/MOPOPGEN/plaw/PRS/CRC/onco_with_rel/impute/phasing_%J_chr%I.stderr
 
-#module load shapeit
 
 chr=${LSB_JOBINDEX} 
+
+#This script uses shapeit3, which is much faster
+#If you'd prefer to use shapeit 2, uncomment the module load, and change command to shapeit
+
+
+#module load shapeit
+
 /scratch/DGE/MOPOPGEN/plaw/programs/shapeit3/shapeit3.r884.1 -B /scratch/DGE/MOPOPGEN/plaw/PRS/CRC/onco_with_rel/impute/prephased/crc_onco_relatives_chr$chr -M /scratch/Dalek/cancgene/mhenrion/1kG_Dec2013_integrated_unzipped/genetic_map_chr$chr\_combined_b37.txt -O /scratch/DGE/MOPOPGEN/plaw/PRS/CRC/onco_with_rel/impute/phased/crc_onco_relatives_chr$chr\.phased -L /scratch/DGE/MOPOPGEN/plaw/PRS/CRC/onco_with_rel/impute/scripts/tmp/phasing_chr$chr --thread 3
