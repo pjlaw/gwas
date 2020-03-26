f0b9a2843bd49a17d886d7a6b1fddbb334385094
Philip Law <philip.law@icr.ac.uk>
2020-03-26 15:37:47 +0000

    Update imiss-vs-het.R

--- qc/imiss-vs-het.R
+++ qc/imiss-vs-het.R
@@ -14,14 +14,17 @@ axis(2,at=c(0,0.05,0.10,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5),tick=T)
 axis(1,at=-6:0, labels=10**(-6:0))
 m=mean(het$meanHet)
 stdev=sd(het$meanHet)
-abline(h=m-(3*stdev),col="RED",lty=2)
-abline(h=m+(3*stdev),col="RED",lty=2)
-abline(v=log10(0.03), col="RED", lty=2)
+
+missingness_threshold=0.05
+n_stdevs=3
+abline(h=m-(n_stdevs*stdev),col="RED",lty=2)
+abline(h=m+(n_stdevs*stdev),col="RED",lty=2)
+abline(v=log10(missingness_threshold), col="RED", lty=2)
 dev.off()
 
-missing=imiss[which(imiss$logF_MISS>log10(0.05)),1]
-hetero1=het[which(het$meanHet<(m-3*stdev)),1]
-hetero2=het[which(het$meanHet>(m+3*stdev)),1]
+missing=imiss[which(imiss$logF_MISS>log10(missingness_threshold)),1]
+hetero1=het[which(het$meanHet<(m-n_stdevs*stdev)),1]
+hetero2=het[which(het$meanHet>(m+n_stdevs*stdev)),1]
 exclude=union(missing, hetero1)
 exclude=union(exclude, hetero2)
 fileConn<-file(paste0(path,"/fail-imisshet-qc.txt"),"w")
