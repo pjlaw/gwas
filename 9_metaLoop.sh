f8183b9bcd7a168f5bdf6059a9c4f4946614f693
Philip Law <philip.law@icr.ac.uk>
2021-07-26 16:01:56 +0100

    Update 9_metaLoop.sh

--- 9_metaLoop.sh
+++ 9_metaLoop.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-#BSUB -J CLL_meta[1-22]
-#BSUB -P zero-houlston
-#BSUB -n 1
-#BSUB -W 120:00
-#BSUB -o /scratch/cancgene/plaw/scripts/tmp/CLL_%J.chr%I_meta_stdout.txt
-#BSUB -e /scratch/cancgene/plaw/scripts/tmp/CLL_%J.chr%I_meta_stderr.txt
+#SBATCH -J meta
+#SBATCH -t 120:00:00
+#SBATCH --array=1-22
+#SBATCH -o meta.%A_%a.out
 
-# set-up run parameters
-i=${LSB_JOBINDEX}
-path="/data/sutst/DGE/MOPOPGEN/DATA/studies/CLL/impute_UK10k_1kG"
+chr=${SLURM_ARRAY_TASK_ID} 
 
-/scratch/cancgene/mhenrion/meta_snptest/meta_static/meta-stat-1.3.2 --snptest --method 1 --threshold 0.4 --cohort $path/CLL1_58BC/filtered/CLL1_filtered_allcols_all_maf_chr$i\.txt $path/CLL2_NBS/filtered/CLL2_filtered_allcols_chr$i\.txt --output /scratch/cancgene/plaw/CLLmeta/meta_random/CLL_meta_random_chr$i\.txt
+path="/data/scratch/DGE/DUDGE/MOPOPGEN/plaw/data_in/"
+/data/scratch/DGE/DUDGE/MOPOPGEN/plaw/programs/meta_v1.7_x86_64_dynamic/meta --method 1 --threshold 0.8 --cohort $path/CLL1_58BC/filtered/CLL1_filtered_allcols_all_maf_chr$i\.txt $path/CLL2_NBS/filtered/CLL2_filtered_allcols_chr$i\.txt --output /scratch/cancgene/plaw/CLLmeta/meta_random/CLL_meta_random_chr$i\.txt
