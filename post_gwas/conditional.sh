7a330d300b2d07729e4a8ca65ce142ce2a4e20a4
Philip Law <philip.law@icr.ac.uk>
2021-08-10 10:45:42 +0100

    Update post_gwas/conditional.sh

--- post_gwas/conditional.sh
+++ post_gwas/conditional.sh
@@ -1,10 +1,18 @@
 chrom=6
+#run the conditional analysis
 /data/scratch/DGE/DUDGE/MOPOPGEN/plaw/programs/gcta_1.93.2beta/gcta64 \
 --bfile /data/scratch/DGE/DUDGE/MOPOPGEN/plaw/CRC_GWAS/oncoarray/imputation/imputed_plink/CRC_ONCO_chr"$chrom"_imputed \
 --chr "$chrom" \
 --maf 0.005 \
---cojo-file /data/scratch/DGE/DUDGE/MOPOPGEN/plaw/CRC_GWAS/US_meta/cojo//input/US_UK_ASN_CRC_info4_mega_merged_chr"$chrom".ma \
---cojo-cond /data/scratch/DGE/DUDGE/MOPOPGEN/plaw/CRC_GWAS/US_meta/cojo//new_snps_20200616_"$chrom"_redo.txt \
---out /data/scratch/DGE/DUDGE/MOPOPGEN/plaw/CRC_GWAS/US_meta/cojo//output/EUR/US_UK_ASN_CRC_chr"$chrom"_new_snps_20200616_conditional_redo
+--cojo-file gwas_chr"$chrom".ma \
+--cojo-cond snplist_"$chrom".txt \
+--out gwas_conditional_chr"$chrom"
+
+conda activate plink1.90b6.18
+#extract the conditioned p-values
+echo "SNP P" > gwas_conditional_chr"$chrom"_plink.in
+awk '{if (NR>1) print $2,$13}'  gwas_conditional_chr"$chrom".cma.cojo >> gwas_conditional_chr"$chrom"_plink.in
+#clump the pvalues using plink - keep only GWS snps
+plink --bfile /scratch/DGE/MOPOPGEN/plaw/reference_data/1kg_uk10k_eur/1kg_uk10k_eur_chr"$chrom"_with_cms --clump gwas_conditional_chr"$chrom"_plink.in --clump-p1 5e-8 --clump-p2 5e-8  --clump-kb 1000 --clump-r2 0.1 --out gwas_conditional_clump_chr"$chrom"
 
 
